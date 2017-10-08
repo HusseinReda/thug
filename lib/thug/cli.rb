f6fe524d505b48c36d2d8555a1969df1a6fbbd6c
sherif <sherif.alaa55@gmail.com>
2017-10-08 21:43:12 +0200

    guideline comments

--- lib/thug/cli.rb
+++ lib/thug/cli.rb
@@ -7,7 +7,16 @@ module Thug
     default_task :list
 
     def list(*entry_names)
-      
+      # detect gem file and create/update gem .lock file
+
+      # detect current os, platform and package manager
+
+      # check internet
+      # if connected, request system dependencies
+
+      # if not, get system dependencies from local file
+
+      # run install command to install dependencies
     rescue => ex
       $stderr.puts ex.message
     end
